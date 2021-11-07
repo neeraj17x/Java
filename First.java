e238079b50734e465f746db314f7780acf49adbe
Neeraj Kumar <neeraj.kumar@instantsys.com>
2021-11-07 11:58:55 +0530

    First commit for Java Repository

---
+++ First.java
@@ -0,0 +1,14 @@
+/**
+ * @author: Neeraj Kumar
+ * @Email: neeraj.kumar@instantsys.com
+ * Description: First Java program on Ubuntu
+ */
+//import java.lang.*;   //Imported by default
+//import java.io.*;
+public class First
+{
+    public static void main(String args[]) throws IOException
+    {
+        System.out.println("Hello, world !\n");
+    }
+}
