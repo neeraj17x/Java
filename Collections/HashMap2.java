c6a0752d839678e572f4a9d36043598b3b69c708
Neeraj Kumar <neeraj.csn@gmail.com>
2025-07-16 19:10:52 +0530

    Collection examples- HashMap Example 2- generic with Object values

--- Collections/HashMap2.java
+++ Collections/HashMap2.java
@@ -1,3 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMap2 {
-    
+    public static void main(String[] args) {
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("name", "Neeraj");
+        map1.put("city", "Patna");
+        System.out.println(map1);
+        map1.put("age", 31);
+        System.out.println(map1);
+        
+    }
 }
