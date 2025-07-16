18a98c481d5f30d38251b68fdd5a7a88f0e27360
Neeraj Kumar <neeraj.csn@gmail.com>
2025-07-16 18:13:17 +0530

    User defined Exception class- modified

--- Basics/Example1.java
+++ Basics/Example1.java
@@ -4,22 +4,18 @@ class Base extends Exception {
 class Derived extends Base  {
    public int num;
 }
- 
-public class Example1 {
+
+public class Example1
+{
   public static void main(String args[]) {
-   // some other stuff
    try {
-       // Some monitored code
-       throw new Derived();
-    }
-   //  catch(Derived d) { 
-   //    System.out.println("Caught derived class exception"); 
-   // }
-    catch(Base b) { 
-       System.out.println("Caught base class exception"+b.bnum); 
-    }
-   //  catch(Derived d) { 
-   //     System.out.println("Caught derived class exception"); 
-   //  }
+      throw new Derived();
+   // } catch(Derived d) {
+   //    System.out.println("Caught derived class exception");
+   } catch(Base b) {
+      System.out.println("Caught base class exception :"+b.bnum); 
+   // } catch(Derived d) { 
+   //    System.out.println("Caught derived class exception");
+   }
   }
 }
