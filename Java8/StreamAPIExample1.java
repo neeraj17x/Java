4634dd7b698718f62cb2002bf2e6d3a599af9311
Neeraj Kumar <neeraj.csn@gmail.com>
2025-09-27 12:44:59 +0530

    Java 8- Stream API examples- changes

--- Java8/StreamAPIExample1.java
+++ Java8/StreamAPIExample1.java
@@ -11,6 +11,8 @@ public class StreamAPIExample1 {
         for(int i=0; i< 5; i++) {
             list.add(i*9+i-9);
         }
+        System.out.println(list);
+        System.out.println("-------------------------------");
 
         // list.forEach(System.out:: println);
         list.stream().forEach(System.out::println);
--- Java8/StreamAPIExample2.java
+++ Java8/StreamAPIExample2.java
@@ -9,31 +9,43 @@ public class StreamAPIExample2 {
         List<Integer> nums = Arrays.asList(24,5,19,12,9,16);
 
         // Stream
-        // Stream<Integer> stream = nums.stream();
+        Stream<Integer> stream = nums.stream();
 
-        // list.stream().forEach(System.out::println); // Single Thread
-        // list.parallelStream().forEach(System.out::println); // Using multiple threads
+        // nums.stream().forEach(System.out::println); // Single Thread
+        // nums.parallelStream().forEach(System.out::println); // Using multiple threads
+
+        System.out.println("----------------------------------");
 
         // stream.forEach(n -> System.out.println(n));
         // stream.forEach(System.out::println);      // Stream data can be used only once.
 
+        System.out.println("----------------------------------");
+
         // Count elements
-        // System.out.println(stream.count());
+        System.out.println("Stream count: " + stream.count());
+        System.out.println("----------------------------------");
 
         // Sorting
         // Stream<Integer> sortedStream = stream.sorted();
         // sortedStream.forEach(System.out::println);
+        System.out.println("----------------------------------");
 
         // Mapping
         // Stream<Integer> mappedStream = stream.map(n -> n*3);
         // mappedStream.forEach(System.out::println);
+        System.out.println("----------------------------------");
 
         // Filtering
         // Stream<Integer> filteredStream = stream.filter(n -> n%2 == 0);
         // filteredStream.forEach(System.out::println);
+        System.out.println("----------------------------------");
 
         // All in One
         // nums.stream().filter(n-> n%2 == 1).sorted().map(n-> n*3).forEach(System.out::println);
+
+        // Stream<Integer> stream2 = nums.stream().filter(n-> n%2 == 1).sorted().map(n-> n*3);
+        // stream2.forEach(System.out::println);
+        System.out.println("----------------------------------");
 
         // Include Reduce
         int res = nums.stream().filter(n-> n%2 == 1).sorted().map(n-> n*3).reduce(0,(c,e) -> c+e);
